c3d7b7ff276e66cfe1b9868e8a5b7799b48e3ddb
yuvrajpuri <151592476+yuvrajpuri@users.noreply.github.com>
2025-03-30 14:50:25 -0400

    Update fin_page.py
    first attempt at integrating machine learning with streamlit.

--- Final Project Streamlit Demo/pages/fin_page.py
+++ Final Project Streamlit Demo/pages/fin_page.py
@@ -1,7 +1,7 @@
 # Final Output Page
 
 import streamlit as st
-#import tensorflow as tf
+import tensorflow as tf
 
 st.title("Final Output Page")
 
@@ -9,13 +9,13 @@ st.write("Not entirely sure what to put here yet. Maybe a combination of everyth
 
 st.write("If there are any suggestions for what to include, please let me know.")
 
-#model = tf.keras.models.Sequential([
-#  tf.keras.layers.Flatten(input_shape=(28, 28)),
-#  tf.keras.layers.Dense(128, activation='relu'),
-#  tf.keras.layers.Dropout(0.2),
-#  tf.keras.layers.Dense(10)
-#])
-#st.write(model.summary())
-#print(type(model.summary()))
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+st.write(model.summary())
+print(type(model.summary()))
 
 # The same problems I had with Jupyter that stopped me from running anything with tensorflow are rearing their head here.
