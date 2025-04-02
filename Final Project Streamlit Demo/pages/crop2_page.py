c7a7b068802cfdcb92e6678cfc30f7bcec55ebe7
yuvrajpuri <151592476+yuvrajpuri@users.noreply.github.com>
2025-04-02 00:57:25 -0400

    Create crop2_page.py
    Initial dummy.

---
+++ Final Project Streamlit Demo/pages/crop2_page.py
@@ -0,0 +1,10 @@
+# Cropping Example page
+import os
+import pandas as pd
+import cv2
+import json
+import streamlit as st
+from PIL import Image
+
+st.header("Cropping Tool Example")
+st.write("Here is where we will showcase the cropping tool developed by another teammate.")
