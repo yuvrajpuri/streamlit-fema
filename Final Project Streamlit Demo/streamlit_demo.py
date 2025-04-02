5d548d60db215d645be4fe363a38131143d25f5a
yuvrajpuri <151592476+yuvrajpuri@users.noreply.github.com>
2025-04-02 00:54:23 -0400

    Update streamlit_demo.py
    Added crop2_page code for testing the cropping tool.

--- Final Project Streamlit Demo/streamlit_demo.py
+++ Final Project Streamlit Demo/streamlit_demo.py
@@ -10,6 +10,13 @@ fin_page = st.Page("pages/fin_page.py", title = "Final Output")
 # Edit : adding a test example page for trying to utilize Object Detection
 ex_page = st.Page("pages/ex_page.py", title = "Example Object Detection")
 
+# Object detection example successful. Need to utilize actual code for it.
+
+# Edit : adding a test example page for trying to use Cropping tool
+crop2_page = st.Page("pages/crop2_page.py", title = "Cropping Tool Examples")
+
+# Note: does not have the implementation yet to account for cropping an image selected yet. 
+
 pg = st.navigation([main_page, ob_page, cap_page, crop_page, geo_page, fin_page, ex_page])
 
 # Run the selected page
