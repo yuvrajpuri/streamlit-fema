71da43ffc45c9e0f7777168c395f92b2b20205bd
yuvrajpuri <151592476+yuvrajpuri@users.noreply.github.com>
2025-04-10 21:47:57 -0400

    Update ob2_page.py
    test of "best" object detection model

--- Final Project Streamlit Demo/pages/ob2_page.py
+++ Final Project Streamlit Demo/pages/ob2_page.py
@@ -7,7 +7,7 @@ import numpy as np
 import os
 
 # Absolute path to the model in Colab - assuming its been pre-uploaded. Need to adjust for a better solution.
-MODEL_PATH = "/content/yolo11x.pt"
+MODEL_PATH = "/content/best.pt"
 
 # Load model once
 @st.cache_resource
