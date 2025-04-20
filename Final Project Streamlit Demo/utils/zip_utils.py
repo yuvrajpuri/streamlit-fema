7724f9d21c7e8d050ae3645ff4fb6bb4ba723a83
yuvrajpuri <151592476+yuvrajpuri@users.noreply.github.com>
2025-04-20 15:49:36 -0400

    Update zip_utils.py
    imported os

--- Final Project Streamlit Demo/utils/zip_utils.py
+++ Final Project Streamlit Demo/utils/zip_utils.py
@@ -2,6 +2,7 @@ import zipfile
 import json
 import io
 from io import BytesIO
+import os
 
 # when making an annotation, clean it of the filetype (e.g. not img1.jpg_annotations but img1_annotations)
 def clean_annotation(filename):
