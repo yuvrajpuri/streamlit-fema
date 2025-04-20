e2e8a2933365b06a9f3c7d2c820c98d6b59a0c5a
yuvrajpuri <151592476+yuvrajpuri@users.noreply.github.com>
2025-04-20 16:58:36 -0400

    Update streamlit_demo.py
    reorganized pages to keep the used pages at the top

--- Final Project Streamlit Demo/streamlit_demo.py
+++ Final Project Streamlit Demo/streamlit_demo.py
@@ -1,32 +1,25 @@
 import streamlit as st
 
 main_page = st.Page("main.py", title="Main Page")
-#ob_page = st.Page("pages/ob_page.py", title="Object Detection")
-cap_page = st.Page("pages/cap_page.py", title = "Captioning (Video Demonstration)")
-#crop_page = st.Page("pages/crop_page.py", title= "Cropping")
-#geo_page = st.Page("geo_page.py", title = "Georeferencing")
-#fin_page = st.Page("pages/fin_page.py", title = "Final Output")
 
+ob2_page = st.Page("pages/ob2_page.py", title = "Object Detection (example)")
 
-
-
-# Edit : adding a test example page for trying to use Cropping tool
-# crop2_page = st.Page("pages/crop2_page.py", title = "Cropping Tool Examples")
-
-#crop3_page = st.Page("pages/crop3_page.py", title = "Cropping using Uploaded Images")
 crop_nococo_page = st.Page("pages/crop_nococo_page.py", title = "Cropping without JSON")
 
-# Note: does not have the implementation yet to account for cropping an image selected yet. 
-
-# Edit : adding a test object detection page for utilizing the YOLO model we have developed
-ob2_page = st.Page("pages/ob2_page.py", title = "Object Detection (example)")
-# ob_nococo_page = st.Page("pages/ob_nococo_page.py", title = "Object Detection No COCO")
-
-# cap2_page = st.Page("pages/cap2_page.py", title = "Captioning in Streamlit (PaliGemma)")
-
 ob_batch = st.Page("pages/ob_batch_page.py", title= "Batch Object Detection")
+
+cap_page = st.Page("pages/cap_page.py", title = "Captioning (Video Demonstration)")
 
 pg = st.navigation([main_page, ob2_page, crop_nococo_page, ob_batch, cap_page])
 
 # Run the selected page
 pg.run()
+
+
+#ob_page = st.Page("pages/ob_page.py", title="Object Detection")
+#crop_page = st.Page("pages/crop_page.py", title= "Cropping")
+#crop2_page = st.Page("pages/crop2_page.py", title = "Cropping Tool Examples")
+#crop3_page = st.Page("pages/crop3_page.py", title = "Cropping using Uploaded Images")
+
+
+
