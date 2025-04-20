c100dd2f8ac1e6a606bfc955b0d77d6957fa8a89
yuvrajpuri <151592476+yuvrajpuri@users.noreply.github.com>
2025-04-19 21:28:47 -0400

    Update streamlit_demo.py
    updated to only include the important / used pages

--- Final Project Streamlit Demo/streamlit_demo.py
+++ Final Project Streamlit Demo/streamlit_demo.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 main_page = st.Page("main.py", title="Main Page")
-ob_page = st.Page("pages/ob_page.py", title="Object Detection")
-cap_page = st.Page("pages/cap_page.py", title = "Captioning")
+#ob_page = st.Page("pages/ob_page.py", title="Object Detection")
+cap_page = st.Page("pages/cap_page.py", title = "Captioning (Video Demonstration)")
 crop_page = st.Page("pages/crop_page.py", title= "Cropping")
-geo_page = st.Page("geo_page.py", title = "Georeferencing")
-fin_page = st.Page("pages/fin_page.py", title = "Final Output")
+#geo_page = st.Page("geo_page.py", title = "Georeferencing")
+#fin_page = st.Page("pages/fin_page.py", title = "Final Output")
 
 
 
@@ -13,7 +13,7 @@ fin_page = st.Page("pages/fin_page.py", title = "Final Output")
 # Edit : adding a test example page for trying to use Cropping tool
 # crop2_page = st.Page("pages/crop2_page.py", title = "Cropping Tool Examples")
 
-crop3_page = st.Page("pages/crop3_page.py", title = "Cropping using Uploaded Images")
+#crop3_page = st.Page("pages/crop3_page.py", title = "Cropping using Uploaded Images")
 crop_nococo_page = st.Page("pages/crop_nococo_page.py", title = "Cropping without JSON")
 
 # Note: does not have the implementation yet to account for cropping an image selected yet. 
@@ -26,7 +26,7 @@ ob2_page = st.Page("pages/ob2_page.py", title = "Object Detection (example)")
 
 ob_batch = st.Page("pages/ob_batch_page.py", title= "Batch Object Detection")
 
-pg = st.navigation([main_page, ob_page, cap_page, crop_page, geo_page, fin_page, ob2_page, crop_nococo_page, ob_batch])
+pg = st.navigation([main_page, ob_page, crop_page, ob2_page, crop_nococo_page, ob_batch, cap_page])
 
 # Run the selected page
 pg.run()
