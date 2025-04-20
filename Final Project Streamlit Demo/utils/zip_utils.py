7dd5e0453974a343a265bef86345d7f2892b4c4c
yuvrajpuri <151592476+yuvrajpuri@users.noreply.github.com>
2025-04-20 13:09:00 -0400

    Create zip_utils.py
    only clean filename at the moment, need to update it

---
+++ Final Project Streamlit Demo/utils/zip_utils.py
@@ -0,0 +1,10 @@
+import zipfile
+import json
+import io
+from io import BytesIO
+
+# when making an annotation, clean it of the filetype (e.g. not img1.jpg_annotations but img1_annotations)
+def clean_annotation(filename):
+    return os.path.splitext(filename)[0]
+
+# Something for making the actual zip file?
