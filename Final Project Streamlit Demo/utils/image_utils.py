879d01985d5a9d33b484726559e744e786e6c3d6
yuvrajpuri <151592476+yuvrajpuri@users.noreply.github.com>
2025-04-20 14:38:09 -0400

    Update image_utils.py

--- Final Project Streamlit Demo/utils/image_utils.py
+++ Final Project Streamlit Demo/utils/image_utils.py
@@ -1,10 +1,12 @@
 from PIL import ImageDraw
 from io import BytesIO
 
+# Crop the image using the bounding box
 def crop_bbox(image, bbox):
     x, y, w, h = bbox
     return image.crop((x, y, x + w, y + h))
 
+# Draw bounding boxes on the image
 def draw_bounding_boxes(image, detections):
     draw = ImageDraw.Draw(image)
     for det in detections:
