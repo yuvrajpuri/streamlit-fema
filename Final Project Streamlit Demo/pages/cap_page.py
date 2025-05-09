7e357b13355ecb5708b71443cc5a530e83417100
yuvrajpuri <151592476+yuvrajpuri@users.noreply.github.com>
2025-05-08 20:57:16 -0400

    Update cap_page.py

--- Final Project Streamlit Demo/pages/cap_page.py
+++ Final Project Streamlit Demo/pages/cap_page.py
@@ -2,10 +2,9 @@ import streamlit as st
 
 st.title("Captioning Tool (Coming Soon)")
 
-st.markdown("This page will allow you to generate captions for disaster images using a vision-language model.")
+st.markdown("This page will show you to generate captions for disaster images using our OpenAI GPT-4o vision-language model.")
 
-# Placeholder for video walkthrough
-st.header("Captioning Tool (OpenAI) Demo")
-st.video("https://youtu.be/iZJx1oGc0hc")  # Replace with your real URL
+st.header("Captioning Tool (OpenAI GPT-4o) Demo")
+st.video("https://youtu.be/iZJx1oGc0hc")  
 
-
+st.write("Be sure to use the models provided!")
